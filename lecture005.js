a3d0430fd97a4e96a406f6ea4f46ac8c0206ac92
Imtiaz Epu <imtiazepu@programmer.net>
2019-06-24 06:02:12 +0600

    callback function

---
+++ lecture005.js
@@ -0,0 +1,53 @@
+// function hello(name) {
+//   print(name);
+// }
+
+// function print(a) {
+//   console.log(a);
+// }
+
+// hello("Imtiaz");
+
+// function hello(name, print) {
+//   print(name);
+// }
+// hello("imtiaz", function(a) {
+//   console.log("hello " + a);
+// });
+
+// hello("Ahmed", function(b) {
+//   console.log("hi " + b);
+// });
+
+// var me = {
+//   name: "Imtiaz Epu",
+//   age: 32,
+//   email: "info@gmail.com"
+// };
+
+// function print(person, callback) {
+//   console.log("person: " + person.name + "(" + person.age + ")");
+//   if (person.age > 33) {
+//     callback(person.email);
+//   }
+// }
+
+// print(me, function(e) {
+//   console.log("email: " + e);
+// });
+
+// function print(data, callback1, callback2) {
+//   console.log(data);
+//   callback1(data);
+//   callback2(data);
+// }
+
+// print(
+//   "imtiaz Epu",
+//   function(u) {
+//     console.log(u.toUpperCase());
+//   },
+//   function(l) {
+//     console.log(l.toLowerCase());
+//   }
+// );
